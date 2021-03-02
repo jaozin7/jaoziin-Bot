fb5f0de80a78e686ffcc79d0941eb2683f81f8a9
jaozin7 <79337355+jaozin7@users.noreply.github.com>
2021-03-02 18:06:16 -0300

    Add files via upload

---
+++ src/nsfwmenu.js
@@ -0,0 +1,22 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `𝘀𝗹𝘃 resumindo aqui e o menu NSFW
+    ◪ *NSFW*
+  │
+  ▯ ⸎❒ ➪ ${prefix}nsfwbobs
+  ▯ ⸎❒ ➪ ${prefix}randomhentaio
+  ▯ ⸎❒ ➪ ${prefix}nsfwtrap
+  ▯ ⸎❒ ➪ ${prefix}nsfwass
+  ▯ ⸎❒ ➪ ${prefix}nsfwbelly
+  ▯ ⸎❒ ➪ ${prefix}nsfwsidebobs
+  ▯ ⸎❒ ➪ ${prefix}nsfwahegao
+  ▯ ⸎❒ ➪ ${prefix}nsfwthighs
+  ▯ ⸎❒ ➪ ${prefix}nsfwarmpits
+  └─ ❏ ${prefix}nsfwfeets
+
+  _obs para usar esses comandos ative o menu NSFW_\n _Digite_\n ${prefix}*nsfw 1*`
+
+
+
+}
+
+exports.nsfwmenu = nsfwmenu
